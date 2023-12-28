2e288d6b995a56fc2efe069f28d229c01518aed8
Omkardivate <112164147+Omkardivate@users.noreply.github.com>
2023-12-28 22:44:57 +0530

    dat 6,8,9

---
+++ adi&om day9/project/mvcproject/Models/Company.cs
@@ -0,0 +1,7 @@
+namespace mvcproject.Models;
+
+public class Company{
+    public string CompanyName{ get;set;}
+    public string ContactName{ set;get;}
+    public int EmployeeCount{ set; get;}
+}
